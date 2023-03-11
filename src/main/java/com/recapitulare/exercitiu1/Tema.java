951554530d5b55310159b973490441312a454275
Emilia29ylx <yo8ylx@gmail.com>
2023-03-11 19:06:09 +0200

    first commit

--- src/main/java/com/recapitulare/exercitiu1/Tema.java
+++ src/main/java/com/recapitulare/exercitiu1/Tema.java
@@ -8,8 +8,8 @@ public class Tema {
         String line = scanner.nextLine();
         if(line.equals("y")){
             System.out.println("Introdu x si y:");
-            int x= scanner.nextInt();
-            int y= scanner.nextInt();
+            int x=1;
+            int y=1;
             try{
                 x= scanner.nextInt();
                 y= scanner.nextInt();
@@ -20,9 +20,14 @@ public class Tema {
             System.out.println("Z = "+z);
         } else {
             System.out.println("Introdu z:");
-            int z = scanner.nextInt();
+            int z = 1;
             int x = calculX(z);
             int y = calculY(z,x);
+            try{
+                z= scanner.nextInt();
+            } catch (Exception e){
+                System.out.println("Nu e numar");
+            }
             System.out.println("X = " + x);
             System.out.println("Y = " + y);
         }
